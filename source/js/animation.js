cf8bb8cdd79b6589b290c61738d6bf1567cda992
PipZip <37911683+PipZip@users.noreply.github.com>
2021-02-27 15:42:25 +0600

    js

--- source/js/animation.js
+++ source/js/animation.js
@@ -39,18 +39,17 @@ $('.plan__anchor').on('click',function (){
 		onComplete: function () {
 			planText.hide();
 			TweenMax.staggerFromTo(planIcons, 0.4, { alpha: 0, scale: 0.5 }, { alpha: 1, scale: 1, delay: 0.1, overwrite: !0, ease: Back.easeOut }, 0.05);
-			setTimeout(function() {lock = 0; step++;}, 1000);
+			setTimeout(function() {lock = 0; step++;}, 500);
 		},
 	});
 });
 
-$(window).on('wheel', function(e) {
 
+
+eventDelay(window, 'mousewheel', function (down, up) {
 	var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-	var delta = e.originalEvent.deltaY;
 	var planIcons = $('.plan__image > svg');
-	if (delta > 0 && lock == 0 && scrollTop == 0) {
-
+	if (down && lock == 0 && scrollTop == 0){
 		switch (step) {
 			case 1:
 				lock = 1;
@@ -67,17 +66,17 @@ $(window).on('wheel', function(e) {
 
 				TweenMax.to(planAnchor, 0.5, { alpha: 0, scale: 0 });
 				TweenMax.to(planText, 0.2, { alpha: 0 }),
-				TweenMax.to(planText, 0.4, { y: "-100%" }),
-				TweenMax.to(planImage, 0.6, {
-					y: "-".concat(e),
-					clearProps: "all",
-					ease: Power1.easeInOut,
-					onComplete: function () {
-						planText.hide();
-						TweenMax.staggerFromTo(planIcons, 0.4, { alpha: 0, scale: 0.5 }, { alpha: 1, scale: 1, delay: 0.1, overwrite: !0, ease: Back.easeOut }, 0.05);
-						setTimeout(function() {lock = 0; step++;}, 1000);
-					},
-				});
+					TweenMax.to(planText, 0.4, { y: "-100%" }),
+					TweenMax.to(planImage, 0.6, {
+						y: "-".concat(e),
+						clearProps: "all",
+						ease: Power1.easeInOut,
+						onComplete: function () {
+							planText.hide();
+							TweenMax.staggerFromTo(planIcons, 0.4, { alpha: 0, scale: 0.5 }, { alpha: 1, scale: 1, delay: 0.1, overwrite: !0, ease: Back.easeOut }, 0.05);
+							setTimeout(function() {lock = 0; step++;}, 500);
+						},
+					});
 
 				break;
 			case 2:
@@ -92,7 +91,7 @@ $(window).on('wheel', function(e) {
 			default:
 				break;
 		}
-	} else  if (delta < 0 && lock == 0 && scrollTop == 0){
+	} else  if (up && lock == 0 && scrollTop == 0){
 		switch (step) {
 			case 2:
 				lock = 1;
@@ -118,7 +117,7 @@ $(window).on('wheel', function(e) {
 									clearProps: "all",
 									onComplete: function () {
 										TweenMax.to(planAnchor, 0.5, { alpha: 1, scale: 1 });
-										setTimeout(function() {lock = 0; step--;}, 1000);
+										lock = 0; step--;
 									},
 								}
 							);
@@ -134,7 +133,7 @@ $(window).on('wheel', function(e) {
 				TweenMax.to(otherContent, 0.7,{opacity:0, webkitFilter:"blur(10px)"});
 				TweenMax.set(planContent, {height:"100vh"});
 				TweenMax.from(planContent, 0.9,{height:0, onComplete: function () {
-						setTimeout(function() {lock = 0; step--;}, 1000);
+						lock = 0; step--;
 					}
 				});
 				break;
@@ -142,8 +141,8 @@ $(window).on('wheel', function(e) {
 				break;
 		}
 	}
-});
 
+}, 1000);
 
 
 $(window).scroll(function (event) {
@@ -153,5 +152,64 @@ $(window).scroll(function (event) {
 	}
 });
 
+
+
+
+function normalizeWheelSpeed(event) {
+	var normalized;
+	if (event.wheelDelta) {
+		normalized = (event.wheelDelta % 120 - 0) == -0 ? event.wheelDelta / 120 : event.wheelDelta / 12;
+	} else {
+		var rawAmmount = event.deltaY ? event.deltaY : event.detail;
+		normalized = -(rawAmmount % 3 ? rawAmmount * 10 : rawAmmount / 3);
+	}
+	return normalized;
+}
+
+function eventDelay(el, eventName, callback, delay) {
+	var currentTime = (new Date()).getTime();
+	var container = typeof el == 'object' ? $(el) : $(el);
+	delay = delay || 1000;
+	eventName = eventName || 'mousewheel';
+	var block = false;
+	if (eventName === 'mousewheel') {
+		var indicator = new WheelIndicator({
+			elem: document.querySelector('body'),
+			preventMouse: false,
+			callback: function(e){
+				var nowTime = (new Date()).getTime();
+				var diff = Math.abs((nowTime - currentTime) / delay);
+				if (diff < 1) {
+				} else {
+					var up = e.direction == 'up';
+					var down = e.direction == 'down';
+					callback(down, up);
+					currentTime = nowTime;
+				}
+			}
+		});
+	} else {
+		container.on(eventName, function (event) {
+			var nowTime = (new Date()).getTime();
+			var diff = Math.abs((nowTime - currentTime) / delay);
+			if (diff < 1.3) return;
+			currentTime = nowTime;
+			if (eventName == 'mousewheel') {
+				var normalized = normalizeWheelSpeed(event)
+				var down = normalized > 0;
+				var up = normalized < 0;
+				callback(down, up);
+			} else if (eventName == 'keyup') {
+				var normalized = event.keyCode
+				var down = normalized == 40 || normalized == 39;
+				var up = normalized == 38 || normalized == 37;
+				callback(down, up);
+			} else {
+				callback();
+			}
+		});
+	}
+
+}
 
 
